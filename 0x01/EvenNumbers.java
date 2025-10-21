670eeb82afef3311acc9142355129b17619cd6e3
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-21 17:28:44 +0400

    Update EvenNumbers.java

--- 0x01/EvenNumbers.java
+++ 0x01/EvenNumbers.java
@@ -1,1 +1,10 @@
-
+public class EvenNumbers{
+    public static void main(String[] args) {
+        for(int i=0; i<100; i++) {
+            if(i%2==0){
+                if(i==98) System.out.println(i);
+                else System.out.print(i+", ");
+            }
+        }
+    }
+}
