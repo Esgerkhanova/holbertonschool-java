9b7f2a55aa07b75d94984ab7c90179f9fb2ab8a6
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-21 22:43:23 +0400

    Update Program.java

--- 0x02/division/Program.java
+++ 0x02/division/Program.java
@@ -1,1 +1,15 @@
+public class Program {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 4;
+        b = 3;
+        TheNumber.todivide(a, b);
+        System.out.println();
+
+        a = 9;
+        b = 0;
+        TheNumber.todivide(a, b);
+    }
+}
 
