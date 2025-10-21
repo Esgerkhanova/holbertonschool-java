d5df20d29cfd5a31530d47615d0073bee6bd7eb7
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-21 16:33:36 +0400

    Fix class declaration and format output statement

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,6 +1,4 @@
-public class PrintIntegerVariable.java {
+public class PrintIntegerVariable{
     public static void main(String[] args) {
         int number = 1600;
-        System.out.println("Pennsylvania Avenue, " +number);
-    }
-}
+        System.out.println("Pennsylvania Avenue, " +number);}}
