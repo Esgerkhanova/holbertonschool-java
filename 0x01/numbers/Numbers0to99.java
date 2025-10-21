1bd093de9fa715d86bcd971e5efbdee171345112
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-21 22:26:35 +0400

    Update Numbers0to99.java

--- 0x01/numbers/Numbers0to99.java
+++ 0x01/numbers/Numbers0to99.java
@@ -1,8 +1,13 @@
-public class Numbers0to99{
-public static void main(String[] arg){
-     for(int i=0; i<100; i++) {
-            if(i==99) {System.out.print(i);}
-            else {System.out.print(i+", ");}
+public class Numbers0to99 {
+    public static void main(String[] args) {
+        String result = "";
+        for (int i = 0; i < 100; i++) {
+            result += i;
+            if (i < 99) {
+                result += ", ";
+            }
         }
+        System.out.println(result);
     }
 }
+
