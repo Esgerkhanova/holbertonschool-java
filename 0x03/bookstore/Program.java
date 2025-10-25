31973920ebac3aafe5f5e921dad771557b7993c5
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-25 19:41:12 +0400

    Update Program.java

--- 0x03/bookstore/Program.java
+++ 0x03/bookstore/Program.java
@@ -1,43 +1,27 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
-        try {
-            Book book1 = new Book("Robinson Crusoe", "Daniel Defoe", 56);
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book1.getTitle(), book1.getAuthor(), book1.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+        Locale.setDefault(Locale.FRANCE);
 
         try {
-            GoldEditionBook gold1 = new GoldEditionBook("Frankenstein", "Mary Shelley", 38.30);
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    gold1.getTitle(), gold1.getAuthor(), gold1.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+            Book b1 = new Book("Robinson Crusoe", "Daniel Defoe", 56.00);
+            b1.printDetails();
 
-        try {
-            GoldEditionBook gold2 = new GoldEditionBook("Alice’s Adventures in Wonderland", "Lewis Carroll", 21.88);
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    gold2.getTitle(), gold2.getAuthor(), gold2.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+            Book b2 = new Book("Frankenstein", "Mary Shelley", 49.79);
+            b2.printDetails();
 
-        try {
-            Book book2 = new Book("Dracula", "Bram Stoker", 15.99);
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book2.getTitle(), book2.getAuthor(), book2.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+            Book b3 = new Book("", "Lewis Carroll", 28.44); 
+            b3.printDetails();
 
-        try {
-            Book book3 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 0);
-            System.out.printf("Title: %s - Author: %s - Price: %.2f\n",
-                    book3.getTitle(), book3.getAuthor(), book3.getPrice());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
+            Book b4 = new Book("Dracula", "", 15.99); 
+            b4.printDetails();
+
+            Book b5 = new Book("Some Book", "Unknown", -10); 
+            b5.printDetails();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
