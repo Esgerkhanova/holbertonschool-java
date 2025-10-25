a29f3620ee66d011a311fca4d23f1b0ed7193121
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-25 19:50:31 +0400

    Update Program.java

--- 0x03/special_account/Program.java
+++ 0x03/special_account/Program.java
@@ -1,5 +1,9 @@
+import java.util.Locale;
+
 public class Program {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.FRANCE); 
+
         try {
             FeeBasedBankAccount acc1 = new FeeBasedBankAccount("AAA", 30.0);
             System.out.printf("Account %s - balance: %.2f\n", acc1.getAccountNumber(), acc1.getBalance());
@@ -10,7 +14,7 @@ public class Program {
             acc1.deposit(10);
             System.out.printf("Account %s - balance: %.2f\n", acc1.getAccountNumber(), acc1.getBalance());
         } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
+            System.out.println(ex.getMessage());
         }
 
         try {
@@ -23,7 +27,20 @@ public class Program {
             acc2.applyMonthlyUpdate();
             System.out.printf("Account %s - balance: %.2f\n", acc2.getAccountNumber(), acc2.getBalance());
         } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            ControlledBankAccount acc3 = new ControlledBankAccount("CCC", 30.0, 20, 5);
+            System.out.printf("Account %s - balance: %.2f\n", acc3.getAccountNumber(), acc3.getBalance());
+            acc3.deposit(125);
+            System.out.printf("Account %s - balance: %.2f\n", acc3.getAccountNumber(), acc3.getBalance());
+            acc3.withdraw(106);
+            System.out.printf("Account %s - balance: %.2f\n", acc3.getAccountNumber(), acc3.getBalance());
+            acc3.applyMonthlyUpdate();
+            System.out.printf("Account %s - balance: %.2f\n", acc3.getAccountNumber(), acc3.getBalance());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
