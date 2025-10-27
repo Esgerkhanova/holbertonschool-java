f6d74e87f159f23e870075acba725424c908ec05
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-27 21:23:19 +0400

    Update CharacterGame.java

--- Classes_Objects/character/CharacterGame.java
+++ Classes_Objects/character/CharacterGame.java
@@ -1,23 +1,4 @@
 public class CharacterGame {
-  
-    private int currentHealth;
-    private String name;
-
-    public int getCurrentHealth() {
-        return currentHealth;
-    }
-
-
-    public void setCurrentHealth(int currentHealth) {
-        this.currentHealth = currentHealth;
-    }
-
- 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public int currentHealth;
+    public String name;
 }
