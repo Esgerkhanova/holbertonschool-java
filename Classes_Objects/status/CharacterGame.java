f283cdb1fc69923c2ce818995e70179c8eee64ad
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-27 21:21:42 +0400

    Update CharacterGame.java

--- Classes_Objects/status/CharacterGame.java
+++ Classes_Objects/status/CharacterGame.java
@@ -1,56 +1,49 @@
 public class CharacterGame {
-    private int currentHealth;
     private String name;
-    private String status;
+    private int currentHealth;
+    private String status; 
 
-  
-    public CharacterGame(int initialHealth, String initialName) {
-        this.name = "Unknown Character"; 
-        setName(initialName);
-     
-        setCurrentHealth(initialHealth);
+
+    public void setName(String name) {
+        this.name = name;
     }
 
-
-
-    public int getCurrentHealth() {
-        return currentHealth;
-    }
-
-    public void setCurrentHealth(int currentHealth) {
-        int clampedHealth = Math.min(100, Math.max(0, currentHealth));
-        this.currentHealth = clampedHealth;
-        if (this.currentHealth > 0) {
-            this.status = "alive";
-        } else {
-            this.status = "dead";
-        }
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        // Validation: Reject null or empty strings
-        if (name != null && !name.trim().isEmpty()) {
-            this.name = name;
+    
+    public void setCurrentHealth(int currentHealth) {
+        if (currentHealth < 0) {
+            this.currentHealth = 0;
+        } else {
+            this.currentHealth = currentHealth;
+        }
+
+        if (this.currentHealth == 0) {
+            this.status = "dead";
+        } else {
+            this.status = "alive";
         }
     }
 
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+  
     public String getStatus() {
         return status;
     }
 
-    // --- Methods ---
 
-    public void takeDamage(int damageAmount) {
-        int newHealth = this.currentHealth - damageAmount;
-        setCurrentHealth(newHealth);
+    public void takeDamage(int damage) {
+        setCurrentHealth(this.currentHealth - damage);
     }
 
-    public void receiveHealing(int healingAmount) {
-        int newHealth = this.currentHealth + healingAmount;
-        setCurrentHealth(newHealth);
+   
+    public void receiveHealing(int heal) {
+        setCurrentHealth(this.currentHealth + heal);
     }
 }
