80647899bc9131ba2aed47611d2495e02c718fb4
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-10-27 21:06:39 +0400

    Create CharacterGame.java

---
+++ Classes_Objects/consistency/CharacterGame.java
@@ -0,0 +1,54 @@
+public class CharacterGame {
+    private int currentHealth;
+    private String name;
+    private String status;
+
+    public CharacterGame(int initialHealth, String initialName) {
+        this.name = "Unknown"; 
+        setName(initialName); 
+        setCurrentHealth(initialHealth);
+    }
+
+
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(int currentHealth) {
+        int clampedHealth = Math.min(100, Math.max(0, currentHealth));
+        this.currentHealth = clampedHealth;
+
+        if (this.currentHealth > 0) {
+            this.status = "alive";
+        } else {
+            this.status = "dead";
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.trim().isEmpty()) {
+            this.name = name;
+        }
+    
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+
+    public void takeDamage(int damageAmount) {
+        int newHealth = this.currentHealth - damageAmount;
+        setCurrentHealth(newHealth);
+    }
+
+    public void receiveHealing(int healingAmount) {
+        int newHealth = this.currentHealth + healingAmount;
+        setCurrentHealth(newHealth);
+    }
+}
