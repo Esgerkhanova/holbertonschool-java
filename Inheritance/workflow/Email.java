e75027ec9b238e36af32011743a1c09faee5a806
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-11-01 23:10:33 +0400

    Delete Inheritance/workflow directory

--- Inheritance/workflow/Email.java
+++
@@ -1,6 +0,0 @@
-public class Email implements NotificationChannel {
-    @Override
-    public void notify(Message message) {
-        System.out.println("[EMAIL] {" + message.getMessageType() + "} - " + message.getText());
-    }
-}
--- Inheritance/workflow/Teams.java
+++
@@ -1,6 +0,0 @@
-public class Teams implements NotificationChannel {
-    @Override
-    public void notify(Message message) {
-        System.out.println("[TEAMS] {" + message.getMessageType() + "} - " + message.getText());
-    }
-}
