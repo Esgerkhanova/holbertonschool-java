9dcdc10b086e3589001c3b69f265a96f30d7b84c
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-11-06 18:17:21 +0400

    Create OrderProcessor.java

---
+++ Interfaces_Exercises/shipping/OrderProcessor.java
@@ -0,0 +1,19 @@
+import providers.*;
+
+public class OrderProcessor {
+
+    private ShippingProvider shippingProvider;
+
+    public OrderProcessor(ShippingProvider shippingProvider) {
+        this.shippingProvider = shippingProvider;
+    }
+
+    public void process(Order order) {
+        Shipping shipping = shippingProvider.calculateShipping(
+                order.getWeight(),
+                order.getTotal()
+        );
+
+        order.setShipping(shipping);
+    }
+}
