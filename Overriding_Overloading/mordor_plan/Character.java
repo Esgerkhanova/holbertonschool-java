2a1dd9ceefdf553f980791e47ac3fdb41f08da4d
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-11-06 18:34:01 +0400

    Create Character.java

---
+++ Overriding_Overloading/mordor_plan/Character.java
@@ -0,0 +1,29 @@
+import food.Food;
+import mood.*;
+
+public class Character {
+
+    private int happinessPoints = 0;
+
+    public void eat(Food[] foods) {
+        for (Food food : foods) {
+            happinessPoints += food.getHappinessPoints();
+        }
+    }
+
+    public Mood getCurrentMood() {
+        if (happinessPoints < -5)
+            return new Angry();
+        else if (happinessPoints >= -5 && happinessPoints <= 0)
+            return new Sad();
+        else if (happinessPoints >= 1 && happinessPoints <= 15)
+            return new Happy();
+        else
+            return new VeryHappy();
+    }
+
+    @Override
+    public String toString() {
+        return happinessPoints + " - " + getCurrentMood().getClass().getSimpleName();
+    }
+}
