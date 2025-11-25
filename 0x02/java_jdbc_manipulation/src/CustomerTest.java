0877f1590d713a77aec132d5cc810396d8961bb0
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-11-25 12:59:53 +0400

    Add CustomerTest class for JDBC operations

---
+++ 0x02/java_jdbc_manipulation/src/CustomerTest.java
@@ -0,0 +1,37 @@
+package src;
+
+public class CustomerTest {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:sqlite:sqlite_database_marco_2022.db";
+
+        CustomerDAOImpl customerDAOImpl = new CustomerDAOImpl();
+        customerDAOImpl.connect(url);
+
+        customerDAOImpl.createTable(url);
+
+        Customer c1 = new Customer();
+        c1.setName("Danilo R. Pereira");
+        c1.setAge(37);
+        c1.setCpf("111.111.111-11");
+        c1.setRg("222.222.222-22");
+
+        Customer c2 = new Customer();
+        c2.setName("Joao Oliveira Silva");
+        c2.setAge(55);
+        c2.setCpf("888.111.111-11");
+        c2.setRg("777.222.222-22");
+
+        customerDAOImpl.insert(url, c1);
+        customerDAOImpl.insert(url, c2);
+
+        customerDAOImpl.selectAll(url);
+
+        customerDAOImpl.update(url, 1, "Danilo Rodrigues Pereira", 38);
+        customerDAOImpl.selectAll(url);
+
+        customerDAOImpl.delete(url, 2);
+        customerDAOImpl.selectAll(url);
+    }
+}
