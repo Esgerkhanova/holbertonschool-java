ab46257ecc67689b41530aeafdaa5ee417fe099f
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-02 22:13:27 +0400

    Update Contact.java

--- Enum/mobile/Contact.java
+++ Enum/mobile/Contact.java
@@ -1,27 +1,36 @@
-public class Contact{
-  private String name;
-  private String phoneNumber;
-  private NumberType type;
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private NumberType type;
 
-  public Contact(String name, String phoneNumber, NumberType type){
-    this.name=name;
-    this.phoneNumber=phoneNumber;
-    this.type=type;
-  }
-  
-public String getName(){
-  return name;
-}
-  public String getPhoneNumber()}
-    return phoneNumber;
-}
+    public Contact(String name, String phoneNumber, NumberType type) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.type = type;
+    }
 
-public Syring getType(){
-  return type;
-}
+    public String getName() {
+        return name;
+    }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-@Override
-  public String toString(){
-  return name+" -> "+phoneNumber+" ("+type+")";
+    public NumberType getType() {
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Contact)) return false;
+        Contact other = (Contact) obj;
+        return this.name.equals(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + phoneNumber + " (" + type + ")";
+    }
 }
