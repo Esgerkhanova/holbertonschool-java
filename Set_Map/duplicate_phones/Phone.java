5b784fa86ba34349a6a8bcacebb55aea8d7e6536
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-14 21:22:13 +0400

    Update Phone.java

--- Set_Map/duplicate_phones/Phone.java
+++ Set_Map/duplicate_phones/Phone.java
@@ -17,8 +17,22 @@ public class Phone {
         return number;
     }
 
-   }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(areaCode, phone.areaCode) &&
+               Objects.equals(number, phone.number);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
+    }
 
-  
-
+    @Override
+    public String toString() {
+        return "(" + areaCode + ") " + number;
+    }
+}
