a6904eb72b4e7cb47b80fd8752215045cbfffae8
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-14 11:57:41 +0400

    Add DuplicatedNumbers class to find duplicates

---
+++ Set_Map/duplicates/DuplicatedNumbers.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+
+public class DuplicatedNumbers {
+
+    public static TreeSet<Integer> isFind(int[] numbers) {
+        HashSet<Integer> seen = new HashSet<>();
+        TreeSet<Integer> duplicates = new TreeSet<>();
+
+        for (int num : numbers) {
+            if (!seen.add(num)) {
+                duplicates.add(num);
+            }
+        }
+
+        return duplicates;
+    }
+}
