321fc2b2263df3d7eb244e1889ac6b4fc18e9ea0
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-14 12:10:25 +0400

    Implement Phone class with equals and hashCode methods

---
+++ Set_Map/duplicate_phones/Phone.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+public class Phone {
+    private String areaCode;
+    private String number;
+
+    public Phone(String areaCode, String number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + areaCode + ") " + number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Phone phone = (Phone) obj;
+        return areaCode.equals(phone.areaCode) &&
+               number.equals(phone.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(areaCode, number);
+    }
+}
