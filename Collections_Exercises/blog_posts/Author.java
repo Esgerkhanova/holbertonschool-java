631fd837211081f3af839a1f82de38f31cfc3d4d
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-14 21:34:07 +0400

    Create Author.java

---
+++ Collections_Exercises/blog_posts/Author.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Author implements Comparable<Author> {
+    private String name;
+    private String surname;
+
+    public Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
+    }
+
+    @Override
+    public int compareTo(Author other) {
+        int byName = this.name.compareToIgnoreCase(other.name);
+        if (byName != 0) return byName;
+        return this.surname.compareToIgnoreCase(other.surname);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author)) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) &&
+               Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+}
