495921816b9850e8ee8356433f19bb9b21446773
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-15 23:12:06 +0400

    Update Post.java

--- Collections_Exercises/blog_posts/Post.java
+++ Collections_Exercises/blog_posts/Post.java
@@ -3,64 +3,55 @@ import java.util.Objects;
 public class Post implements Comparable<Post> {
     private Author author;
     private String title;
-    private String body;
+    private String description;
     private Categories category;
 
-    public Post(Author author, String title, String body, Categories category) {
+    public Post(Author author, String title, String description, Categories category) {
         this.author = author;
         this.title = title;
-        this.body = body;
+        this.description = description;
         this.category = category;
     }
 
-    public Author getAuthor() {
-        return author;
-    }
+    public Author getAuthor() { return author; }
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public Categories getCategory() { return category; }
 
-    public String getTitle() {
-        return title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public Categories getCategory() {
-        return category;
-    }
-
-    // must print only the title
+    // MUST return <title>
     @Override
     public String toString() {
         return title;
     }
 
-    // sort posts by title
+    // Sort by title (required), then tie-breakers to avoid losing items in TreeSet
     @Override
     public int compareTo(Post other) {
         int byTitle = this.title.compareToIgnoreCase(other.title);
         if (byTitle != 0) return byTitle;
 
-        // tie-breaker to keep TreeSet comparator consistent
-        // (same title but different author)
         int byAuthor = this.author.compareTo(other.author);
         if (byAuthor != 0) return byAuthor;
 
-        return String.valueOf(this.category).compareTo(String.valueOf(other.category));
+        int byCategory = this.category.compareTo(other.category);
+        if (byCategory != 0) return byCategory;
+
+        return this.description.compareToIgnoreCase(other.description);
     }
 
-    // equality for "same author + same title" (required duplicate rule)
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Post)) return false;
         Post post = (Post) o;
         return Objects.equals(author, post.author) &&
-               Objects.equals(title, post.title);
+               Objects.equals(title, post.title) &&
+               Objects.equals(description, post.description) &&
+               category == post.category;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(author, title);
+        return Objects.hash(author, title, description, category);
     }
 }
