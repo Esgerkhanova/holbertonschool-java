b1b0afb3826eb96f48d07f4d7c440250c7aec3bf
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-15 23:11:48 +0400

    Update Author.java

--- Collections_Exercises/blog_posts/Author.java
+++ Collections_Exercises/blog_posts/Author.java
@@ -4,19 +4,13 @@ public class Author implements Comparable<Author> {
     private String name;
     private String surname;
 
-    // MUST be exactly this signature for the checker:
     public Author(String name, String surname) {
         this.name = name;
         this.surname = surname;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
+    public String getName() { return name; }
+    public String getSurname() { return surname; }
 
     @Override
     public String toString() {
@@ -35,8 +29,7 @@ public class Author implements Comparable<Author> {
         if (this == o) return true;
         if (!(o instanceof Author)) return false;
         Author author = (Author) o;
-        return Objects.equals(name, author.name) &&
-               Objects.equals(surname, author.surname);
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
     }
 
     @Override
