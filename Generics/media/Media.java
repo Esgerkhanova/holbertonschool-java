051ba92cc725b6ec3ae762817dbe005e2950c7a2
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-17 17:42:49 +0400

    Update Media.java

--- Generics/media/Media.java
+++ Generics/media/Media.java
@@ -1,22 +1,20 @@
-public class Media{
-  private Srting name;
+public class Media {
+    private String name;
 
-public Media(String name){
-  this.name=name;
+    public Media(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Type: %s - Name: %s",
+                this.getClass().getSimpleName(),
+                getName()
+        );
+    }
 }
-
-public String setName(){
-  return name;
-}
-
-
-  @Override
-  public String toString(){
-    return String.format"(
-      "Type: %s - Name: %s",
-      this.getClass().getSimpleName(),
-      getName()
-      );
-  }
-}
-
