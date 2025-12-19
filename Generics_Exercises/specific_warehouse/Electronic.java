646650b6a62cd730de78c9a78578c23d90941e8f
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-19 21:54:11 +0400

    Create Electronic.java

---
+++ Generics_Exercises/specific_warehouse/Electronic.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+
+public class Electronic {
+    private String description;
+    private double value;
+
+    public Electronic(String description, double value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.GERMANY, "[%s] $ %.6f", description, value);
+
+    }
+}
