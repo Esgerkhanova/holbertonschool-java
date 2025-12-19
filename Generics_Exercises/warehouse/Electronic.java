77392bdb2694347a71aa88d30f33106a68bb3650
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-19 21:46:31 +0400

    Create Electronic.java

---
+++ Generics_Exercises/warehouse/Electronic.java
@@ -0,0 +1,22 @@
+public class Electronic {
+    private String description;
+    private double value;
+
+    public Electronic(String description, double value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] $ %.6f", description, value);
+    }
+}
