ab06c7f5acc994181d7ced2255c75464d375b06e
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-19 21:46:05 +0400

    Create Food.java

---
+++ Generics_Exercises/warehouse/Food.java
@@ -0,0 +1,28 @@
+public class Food {
+    private String name;
+    private double calories;
+    private double price;
+
+    public Food(String name, double calories, double price) {
+        this.name = name;
+        this.calories = calories;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %.6f $ %.6f", name, calories, price);
+    }
+}
