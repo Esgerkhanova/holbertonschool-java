1bcee8918e11cd2d2aed0e28a30b441132bdd181
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-20 22:36:59 +0400

    Update Payroll.java

--- Lambda/net/Payroll.java
+++ Lambda/net/Payroll.java
@@ -1,7 +1,6 @@
 public class Payroll {
 
-    public static double calculateNet(Employee employee,
-                                      SalaryCalculator calculator) {
+    public static double calculateNet(Employee employee, SalaryCalculator calculator) {
         return calculator.calculate(employee.getSalary());
     }
 }
