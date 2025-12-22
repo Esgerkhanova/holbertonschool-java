d5666adbce266bc7e6d386c214d94763f8700fa3
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-22 19:24:50 +0400

    Create ProductSearch.java

---
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ProductSearch {
+
+    public static List<Product> filter(List<Product> products, FilterCriteria criteria) {
+        List<Product> result = new ArrayList<>();
+
+        for (Product product : products) {
+            if (criteria.test(product)) {
+                result.add(product);
+            }
+        }
+        return result;
+    }
+}
