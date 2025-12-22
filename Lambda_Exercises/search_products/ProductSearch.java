1d883b23b9edbbcbb1732fec841336ed7f95ed1b
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-22 20:59:24 +0400

    Update ProductSearch.java

--- Lambda_Exercises/search_products/ProductSearch.java
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -2,9 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ProductSearch {
-    public static List<Product> filter(List<Product> products, FilterCriteria criteria) {
-        List<Product> result = new ArrayList<>();
-        for (Product product : products) {
+    public static List filter(List products, FilterCriteria criteria) {
+        List result = new ArrayList();
+        for (Object obj : products) {
+            Product product = (Product) obj;
             if (criteria.test(product)) {
                 result.add(product);
             }
