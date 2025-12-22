2fd94d17656ac4f14476d4cb6fa5ab832e32f732
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-22 21:06:22 +0400

    Update ProductSearch.java

--- Lambda_Exercises/search_products/ProductSearch.java
+++ Lambda_Exercises/search_products/ProductSearch.java
@@ -2,17 +2,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class ProductSearch {
-    /**
-     * Filters a list of products based on a given criteria.
-     * * @param products The original list of products
-     * @param criteria The FilterCriteria (functional interface) to apply
-     * @return A new List containing only products that pass the test
-     */
     public static List<Product> filter(List<Product> products, FilterCriteria criteria) {
         List<Product> result = new ArrayList<>();
         
         for (Product product : products) {
-            // No manual casting (Product) obj is needed when using Generics
             if (criteria.test(product)) {
                 result.add(product);
             }
