b0a6a268d6e851057324d3a3ec652c397a055da7
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-26 19:44:58 +0400

    Update PeopleQuery.java

--- Streams_Collector_Optional_Exercices/people_count_position/PeopleQuery.java
+++ Streams_Collector_Optional_Exercices/people_count_position/PeopleQuery.java
@@ -1,3 +1,4 @@
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -5,10 +6,23 @@ import java.util.stream.Collectors;
 public class PeopleQuery {
 
     static Map<String, Long> getPeopleCountByPosition(List<Person> people) {
+
         return people.stream()
                 .collect(Collectors.groupingBy(
                         Person::getPosition,
                         Collectors.counting()
+                ))
+                .entrySet()
+                .stream()
+                .sorted((e1, e2) -> Integer.compare(
+                        e2.getKey().length(),
+                        e1.getKey().length()
+                ))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a, b) -> a,
+                        LinkedHashMap::new
                 ));
     }
 }
