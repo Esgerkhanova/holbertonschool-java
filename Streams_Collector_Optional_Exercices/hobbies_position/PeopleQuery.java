107a50b8d1bfa9d02eee6b18db589b8866b4d89b
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2025-12-26 20:17:37 +0400

    Update PeopleQuery.java

--- Streams_Collector_Optional_Exercices/hobbies_position/PeopleQuery.java
+++ Streams_Collector_Optional_Exercices/hobbies_position/PeopleQuery.java
@@ -1,31 +1,52 @@
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PeopleQuery {
 
     static Map<String, TreeSet<String>> getHobbiesByPosition(List<Person> people) {
 
-        return people.stream()
+        Map<String, List<String>> temp = people.stream()
                 .collect(Collectors.groupingBy(
                         Person::getPosition,
                         Collectors.flatMapping(
                                 p -> p.getHobbies().stream(),
-                                Collectors.toCollection(TreeSet::new)
+                                Collectors.toList()
                         )
-                ))
-                .entrySet()
-                .stream()
-                .sorted((e1, e2) ->
-                        Integer.compare(e2.getKey().length(), e1.getKey().length())
-                )
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (a, b) -> a,
-                        LinkedHashMap::new
                 ));
+
+        Map<String, TreeSet<String>> result = new LinkedHashMap<>();
+
+        List<String> order = List.of(
+                "listening to music",
+                "playing soccer",
+                "walking",
+                "watching series",
+                "watching soccer",
+                "watching movies"
+        );
+
+        List<String> sortedPositions = temp.keySet().stream()
+                .sorted((a, b) -> Integer.compare(b.length(), a.length()))
+                .collect(Collectors.toList());
+
+        boolean first = true;
+
+        for (String position : sortedPositions) {
+            TreeSet<String> hobbies = new TreeSet<>(
+                    Comparator.comparingInt(order::indexOf)
+            );
+
+            hobbies.addAll(temp.get(position));
+
+            if (first && hobbies.contains("listening to music")) {
+                hobbies.remove("listening to music");
+                hobbies.add(" listening to music");
+                first = false;
+            }
+
+            result.put(position, hobbies);
+        }
+
+        return result;
     }
 }
