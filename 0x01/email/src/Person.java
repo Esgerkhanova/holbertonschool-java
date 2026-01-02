2a6a3cf0bc1939fa6964951667a66251fa8186ec
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-01-02 22:34:40 +0400

    Create Person.java

---
+++ 0x01/email/src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+
+    public static boolean isEmailValid(String email) {
+
+        if (email == null) {
+            return false;
+        }
+
+        if (!email.contains("@")) {
+            return false;
+        }
+
+  
+        if (email.length() > 50) {
+            return false;
+        }
+
+        return true;
+    }
+}
