c04ad2d5e906c98346abb2de7156393a8184b715
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-01-02 22:35:01 +0400

    Create EmailTest.java

---
+++ 0x01/email/src/EmailTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class EmailTest {
+
+    @Test
+    void test_email_with_at_symbol() {
+        assertTrue(Person.isEmailValid("email_test@domain.com"));
+    }
+
+    @Test
+    void test_email_without_at_symbol() {
+        assertFalse(Person.isEmailValid("email_testdomain.com"));
+    }
+
+    @Test
+    void test_email_over_50_characters() {
+        assertEquals(
+                false,
+                Person.isEmailValid(
+                        "email_test_very_long_should_not_be_valid@domain.com"
+                )
+        );
+    }
+}
