dbb79c8dcbea725288e5debb58cb6bea6ba43c07
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-01-06 21:51:44 +0400

    Update PersonModel.java

--- jpa_hibernate/src/main/java/models/PersonModel.java
+++ jpa_hibernate/src/main/java/models/PersonModel.java
@@ -2,13 +2,16 @@ package com.techcamps.registrations.models;
 
 import com.techcamps.registrations.entities.Person;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class PersonModel {
 
     private EntityManager getEntityManager() {
-        return Persistence.createEntityManagerFactory("admin-jpa").createEntityManager();
+        return Persistence
+                .createEntityManagerFactory("admin-jpa")
+                .createEntityManager();
     }
 
     public void create(Person p) {
@@ -17,6 +20,30 @@ public class PersonModel {
         em.persist(p);
         em.getTransaction().commit();
         em.close();
+    }
+
+    public void update(Person p) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        em.merge(p);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public void delete(Person p) {
+        EntityManager em = getEntityManager();
+        em.getTransaction().begin();
+        p = em.merge(p);
+        em.remove(p);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+    public Person findById(Long id) {
+        EntityManager em = getEntityManager();
+        Person p = em.find(Person.class, id);
+        em.close();
+        return p;
     }
 
     public List<Person> findAll() {
