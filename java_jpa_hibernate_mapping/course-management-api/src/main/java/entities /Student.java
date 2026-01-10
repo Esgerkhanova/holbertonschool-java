00eb843a9d622f1fe5978d5ffb4e5c9358737e78
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-01-10 15:57:51 +0400

    Update Student.java

--- java_jpa_hibernate_mapping/course-management-api/src/main/java/entities /Student.java
+++ java_jpa_hibernate_mapping/course-management-api/src/main/java/entities /Student.java
@@ -13,12 +13,23 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-  
+
+    @Column(nullable = false)
+    private String fullName;
+
+
+    @Column(nullable = false, unique = true)
+    private String registration;
+
+    
     @Temporal(TemporalType.DATE)
     private Date birthDate;
 
+
     @Column(nullable = false)
-    private String name;
+    private String email;
+
+
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Address> addresses = new ArrayList<>();
@@ -29,14 +40,19 @@ public class Student {
     @ManyToMany(mappedBy = "students")
     private List<Course> courses = new ArrayList<>();
 
+    
+
     public Student() {}
 
-    public Student(String name, Date birthDate) {
-        this.name = name;
+    public Student(String fullName, String registration, Date birthDate, String email) {
+        this.fullName = fullName;
+        this.registration = registration;
         this.birthDate = birthDate;
+        this.email = email;
     }
 
-   
+ 
+
     public void addAddress(Address address) {
         addresses.add(address);
         address.setStudent(this);
@@ -54,14 +70,20 @@ public class Student {
         }
     }
 
- 
+
     public Long getId() { return id; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getRegistration() { return registration; }
+    public void setRegistration(String registration) { this.registration = registration; }
 
     public Date getBirthDate() { return birthDate; }
     public void setBirthDate(Date birthDate) { this.birthDate = birthDate; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     public List<Address> getAddresses() { return addresses; }
     public List<Phone> getPhones() { return phones; }
