4012c87155093121a4652986a5744692582cec5d
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-01-15 23:27:59 +0400

    Update Student.java

--- java_jpa_hibernate_mapping/course-management-api/src/main/java/entities /Student.java
+++ java_jpa_hibernate_mapping/course-management-api/src/main/java/entities /Student.java
@@ -6,86 +6,40 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "students")
 public class Student {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
-    @Column(nullable = false)
     private String fullName;
-
-
-    @Column(nullable = false, unique = true)
     private String registration;
 
-    
     @Temporal(TemporalType.DATE)
     private Date birthDate;
 
-
-    @Column(nullable = false)
     private String email;
 
-
-
-    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "student_id")
     private List<Address> addresses = new ArrayList<>();
 
-    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "student_id")
     private List<Phone> phones = new ArrayList<>();
 
-    @ManyToMany(mappedBy = "students")
-    private List<Course> courses = new ArrayList<>();
-
     
-
-    public Student() {}
-
-    public Student(String fullName, String registration, Date birthDate, String email) {
-        this.fullName = fullName;
-        this.registration = registration;
-        this.birthDate = birthDate;
-        this.email = email;
-    }
-
- 
-
-    public void addAddress(Address address) {
-        addresses.add(address);
-        address.setStudent(this);
-    }
-
-    public void addPhone(Phone phone) {
-        phones.add(phone);
-        phone.setStudent(this);
-    }
-
-    public void addCourse(Course course) {
-        courses.add(course);
-        if (!course.getStudents().contains(this)) {
-            course.getStudents().add(this);
-        }
-    }
-
-
     public Long getId() { return id; }
-
+    public void setId(Long id) { this.id = id; }
     public String getFullName() { return fullName; }
     public void setFullName(String fullName) { this.fullName = fullName; }
-
     public String getRegistration() { return registration; }
     public void setRegistration(String registration) { this.registration = registration; }
-
     public Date getBirthDate() { return birthDate; }
     public void setBirthDate(Date birthDate) { this.birthDate = birthDate; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-
     public List<Address> getAddresses() { return addresses; }
+    public void setAddresses(List<Address> addresses) { this.addresses = addresses; }
     public List<Phone> getPhones() { return phones; }
-    public List<Course> getCourses() { return courses; }
+    public void setPhones(List<Phone> phones) { this.phones = phones; }
 }
