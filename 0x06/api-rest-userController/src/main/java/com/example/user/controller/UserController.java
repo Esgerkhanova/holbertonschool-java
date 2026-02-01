ec3b618aa13c8905a62949bb6f3051204e7dc669
Aybeniz Esgerxanova <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-01 22:22:51 +0400

    Update UserController.java

--- 0x06/api-rest-userController/src/main/java/com/example/user/controller/UserController.java
+++ 0x06/api-rest-userController/src/main/java/com/example/user/controller/UserController.java
@@ -11,21 +11,25 @@ public class UserController {
 
     @GetMapping("/user-id/{id}")
     public String findUserById(@PathVariable int id) {
-        if (id > 0 && id < 100) return "You have entered a valid ID";
+        if (id > 0 && id < 100) {
+            return "You have entered a valid ID";
+        }
         return "You have entered an invalid ID.";
     }
 
     @GetMapping("/user-name/{username}")
     public String findUserByUserName(@PathVariable String username) {
-        if (username != null && username.length() > 3 && username.length() < 15)
+        if (username != null && username.length() > 3 && username.length() < 15) {
             return "You have entered a valid USERNAME";
+        }
         return "You have entered an invalid USERNAME.";
     }
 
     @GetMapping("/user-cpf/{cpf}")
     public String findUserByCPF(@PathVariable String cpf) {
-        if (cpf != null && cpf.length() > 3 && cpf.length() < 15)
+        if (cpf != null && cpf.length() > 3 && cpf.length() < 15) {
             return "You have entered a valid CPF";
+        }
         return "You have entered an invalid CPF.";
     }
 }
