14296a0e411848cbb147a451798994e126a3c438
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-06 16:17:29 +0400

    Create MageBuilder.java

---
+++ 0x00/character-builder/MageBuilder.java
@@ -0,0 +1,39 @@
+public class MageBuilder implements Builder {
+
+    private String name;
+    private CharacterType type;
+    private int intelligence;
+    private int strength;
+    private int endurance;
+    private int resilience;
+    private int dexterity;
+
+    @Override
+    public void setName(String name) { this.name = name; }
+
+    @Override
+    public void setType(CharacterType type) { this.type = type; }
+
+    @Override
+    public void setIntelligence(int intelligence) { this.intelligence = intelligence; }
+
+    @Override
+    public void setStrength(int strength) { this.strength = strength; }
+
+    @Override
+    public void setEndurance(int endurance) { this.endurance = endurance; }
+
+    @Override
+    public void setResilience(int resilience) { this.resilience = resilience; }
+
+    @Override
+    public void setDexterity(int dexterity) { this.dexterity = dexterity; }
+
+    public Mage build() {
+        // type Director tərəfindən MAGE qoyulacaq; yenə də safety:
+        if (type != null && type != CharacterType.MAGE) {
+            throw new IllegalArgumentException("Invalid attributes for MAGE");
+        }
+        return new Mage(name, intelligence, strength, endurance, resilience, dexterity);
+    }
+}
