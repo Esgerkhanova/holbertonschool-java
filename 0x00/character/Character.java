99021bf237e9be1fb7b1e78ca177d84fa04b966a
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-06 16:53:18 +0400

    Update Character.java

--- 0x00/character/Character.java
+++ 0x00/character/Character.java
@@ -1,5 +1,4 @@
 public abstract class Character {
-
     protected String name;
     protected CharacterType type;
     protected int intelligence;
@@ -8,11 +7,8 @@ public abstract class Character {
     protected int resilience;
     protected int dexterity;
 
-    public Character(String name, CharacterType type,
-                     int intelligence, int strength,
-                     int endurance, int resilience,
-                     int dexterity) {
-
+    public Character(String name, CharacterType type, int intelligence, int strength,
+                     int endurance, int resilience, int dexterity) {
         this.name = name;
         this.type = type;
         this.intelligence = intelligence;
@@ -23,4 +19,12 @@ public abstract class Character {
     }
 
     public abstract double getAttackDamage();
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Character { name = %s, type = %s, intelligence = %d, strength = %d, endurance = %d, resilience = %d, dexterity = %d, attack damage = %.2f }",
+                name, type, intelligence, strength, endurance, resilience, dexterity, getAttackDamage()
+        );
+    }
 }
