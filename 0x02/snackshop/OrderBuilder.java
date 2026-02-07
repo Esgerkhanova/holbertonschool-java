6be4e52f9461ccef861047859afe36e1ca585bab
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-07 16:49:00 +0400

    Create OrderBuilder.java

---
+++ 0x02/snackshop/OrderBuilder.java
@@ -0,0 +1,29 @@
+public class OrderBuilder extends OrderAbstractBuilder {
+
+    private Order order = new Order();
+
+    @Override
+    public void setSnack(SnackType type) {
+        order.addItemInBox(new OrderItem(OrderItemType.SNACK, type.name()));
+    }
+
+    @Override
+    public void setFries(FriesSize size) {
+        order.addItemInBox(new OrderItem(OrderItemType.FRIES, size.name()));
+    }
+
+    @Override
+    public void setToy(ToyType type) {
+        order.addItemInBox(new OrderItem(OrderItemType.TOY, type.name()));
+    }
+
+    @Override
+    public void setDrink(DrinkType type) {
+   
+        order.addItemOutOfBox(new OrderItem(OrderItemType.DRINK, type.name()));
+    }
+
+    public Order build() {
+        return order;
+    }
+}
