863fcc56f6b766fd936a77d9d542732409070261
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-07 16:47:05 +0400

    Create OrderItem.java

---
+++ 0x02/snackshop/OrderItem.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class OrderItem {
+    private OrderItemType type;
+    private String name;
+
+    public OrderItem(OrderItemType type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public OrderItemType getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItem)) return false;
+        OrderItem that = (OrderItem) o;
+        return type == that.type && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, name);
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + name;
+    }
+}
