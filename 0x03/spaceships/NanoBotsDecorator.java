a2d56bb620b6af26c289021a880d76eb1a5fd5fe
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-09 14:23:54 +0400

    Create NanoBotsDecorator.java

---
+++ 0x03/spaceships/NanoBotsDecorator.java
@@ -0,0 +1,11 @@
+public class NanoBotsDecorator extends SpacecraftDecorator {
+
+    public NanoBotsDecorator(Spacecraft spacecraft) {
+        super(spacecraft);
+    }
+
+    @Override
+    public int getAttack() {
+        return spacecraft.getAttack() + 25;
+    }
+}
