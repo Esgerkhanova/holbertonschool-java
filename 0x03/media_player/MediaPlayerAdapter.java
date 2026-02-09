34f7485ab927e93e53008f00b8c6fbc9d5cff1c6
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-09 14:49:07 +0400

    Create MediaPlayerAdapter.java

---
+++ 0x03/media_player/MediaPlayerAdapter.java
@@ -0,0 +1,19 @@
+public class MediaPlayerAdapter implements MediaPlayer {
+
+    private AdvancedMediaPlayer advancedMediaPlayer;
+
+    public MediaPlayerAdapter(MediaType type) {
+        if (type != MediaType.MP3) {
+            this.advancedMediaPlayer = new VideoMediaPlayer();
+        }
+    }
+
+    @Override
+    public void play(MediaType type, String fileName) {
+        if (type == MediaType.VLC) {
+            advancedMediaPlayer.playVLC(fileName);
+        } else if (type == MediaType.MP4) {
+            advancedMediaPlayer.playMP4(fileName);
+        }
+    }
+}
