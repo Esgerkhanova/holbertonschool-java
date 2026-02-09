4123badc8cdcd25a6454bd106b89dc4d53eb8208
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-09 14:58:47 +0400

    Update VideoMediaPlayer.java

--- 0x03/media_player/VideoMediaPlayer.java
+++ 0x03/media_player/VideoMediaPlayer.java
@@ -2,11 +2,11 @@ public class VideoMediaPlayer implements AdvancedMediaPlayer {
 
     @Override
     public void playVLC(String fileName) {
-        System.out.println("Playing VLC: " + fileName);
+        System.out.print("Playing VLC: " + fileName);
     }
 
     @Override
     public void playMP4(String fileName) {
-        System.out.println("Playing MP4: " + fileName);
+        System.out.print("Playing MP4: " + fileName);
     }
 }
