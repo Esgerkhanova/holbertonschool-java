2bb490d95c0f64c8d5188a24b95cfcdd22eab764
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-09 14:24:59 +0400

    Create ArmorDecorator.java

---
+++ 0x03/spaceships/ArmorDecorator.java
@@ -0,0 +1,11 @@
+public class ArmorDecorator extends SpacecraftDecorator {
+
+    public ArmorDecorator(Spacecraft spacecraft) {
+        super(spacecraft);
+    }
+
+    @Override
+    public int getHealth() {
+        return spacecraft.getHealth() + 150;
+    }
+}
