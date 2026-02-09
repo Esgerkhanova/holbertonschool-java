fde3303425e4e2f86380b1bf8ac4eaaabf5c241b
aybeniz <asgarxanova.aybaniz.elmaddin.2024@unec.edu.az>
2026-02-09 14:49:25 +0400

    Create AudioPlayer.java

---
+++ 0x03/media_player/AudioPlayer.java
@@ -0,0 +1,12 @@
+public class AudioPlayer implements MediaPlayer {
+
+    @Override
+    public void play(MediaType type, String fileName) {
+        if (type == MediaType.MP3) {
+            System.out.println("Playing MP3: " + fileName);
+        } else {
+            MediaPlayerAdapter adapter = new MediaPlayerAdapter(type);
+            adapter.play(type, fileName);
+        }
+    }
+}
